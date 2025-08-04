7b87d6bdf6365c273bea3b09680bd33177dcea29
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2025-08-04 12:05:07 +0900

    Title: 원소 10개의 합, Time: 170ms, Memory: 15MB, Status: Passed - Codetree

--- 250804/원소 10개의 합/sum-of-10-elements.js
+++ 250804/원소 10개의 합/sum-of-10-elements.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
-const arr = fs.readFileSync(0).toString().trim().split(" ");
+const arr = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-console.log(arr.reduce((prev, cur) => prev + Number(cur), 0))
+console.log(arr.reduce((prev, cur) => prev + cur, 0))
