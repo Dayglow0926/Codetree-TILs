536feb279459678dc7e9abc8d92c03624a821b0b
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2025-08-06 12:26:13 +0900

    Title: 배열의 특정 위치 찾기, Time: 176ms, Memory: 15MB, Status: Passed - Codetree

--- 250806/배열의 특정 위치 찾기/find-specific-location-fo-array.js
+++ 250806/배열의 특정 위치 찾기/find-specific-location-fo-array.js
@@ -6,7 +6,7 @@ const odd = [0,0]
 
 for(let i=0; i<arr.length; i+=1){
     if(i % 2) even += arr[i];
-    if(!(arr[i] % 3)) {
+    if(!((i+1) % 3)) {
         odd[0] += arr[i];
         odd[1]++;
     }
