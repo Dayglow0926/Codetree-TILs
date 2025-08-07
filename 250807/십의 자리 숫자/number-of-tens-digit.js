6a86fa1206e5905ffc5777f98b251ea6205d13fa
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2025-08-07 16:14:22 +0900

    Title: 십의 자리 숫자, Time: 166ms, Memory: 15MB, Status: Passed - Codetree

--- 250807/십의 자리 숫자/number-of-tens-digit.js
+++ 250807/십의 자리 숫자/number-of-tens-digit.js
@@ -3,6 +3,9 @@ const arr = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 const stack = Array(9).fill(0);
 
-arr.forEach(n => stack[parseInt(n / 10) - 1]++);
+for(let n of arr){
+    if(n === 0) break;
+    stack[parseInt(n / 10) - 1]++;
+}
 
 stack.forEach((n,i) => console.log(`${i+1} - ${n}`))
