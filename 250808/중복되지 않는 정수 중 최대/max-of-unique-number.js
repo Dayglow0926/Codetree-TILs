27195f9aed3e4a8fc685a7b96f47e12977f5fb0a
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2025-08-08 17:05:50 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 176ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250808/중복되지 않는 정수 중 최대/max-of-unique-number.js
+++ 250808/중복되지 않는 정수 중 최대/max-of-unique-number.js
@@ -4,12 +4,16 @@ const arr = fs.readFileSync(0).toString().trim().split("\n");
 const nums = arr[1].split(" ").map(Number);
 
 let stack = [nums[0]];
+const dupl = []
 
 for(let i=1; i<nums.length; i++){
     const index = stack.indexOf(nums[i]);
-    if(index === -1) stack.push(nums[i]);
-    else{
+    const duplIndex = dupl.indexOf(nums[i]);
+
+    if(index === -1 && duplIndex === -1) stack.push(nums[i]);
+    else {
         stack.splice(index, 1);
+        dupl.push(nums[i])
     }
 }
 
