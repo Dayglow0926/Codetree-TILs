00d9461c4abb229f7b2206e99ff0a190dd8d2ae8
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2025-08-13 19:21:12 +0900

    Title: 격자 모양 출력하기, Time: 171ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250813/격자 모양 출력하기/print-grid-shape.js
@@ -0,0 +1,15 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().trim().split("\n");
+
+const [N,M] = input[0].split(" ").map(Number);
+
+const arr = Array(N).fill(0).map(() => Array(N).fill(0));
+
+let count = 1;
+
+for(let i=1; i <= M; i++){
+    const [n,m] = input[i].split(" ").map(Number);
+    arr[n-1][m-1] = n * m;
+}
+
+arr.forEach((a) => console.log(a.join(" ")))
