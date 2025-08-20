8046dcfc24e8656fd9490c2447c30041b15ecd44
Dayglow0926 <41177346+Dayglow0926@users.noreply.github.com>
2025-08-20 16:01:06 +0900

    Title: 정수만 추출하기, Time: 175ms, Memory: 15MB, Status: Passed - Codetree

--- 250820/정수만 추출하기/extract-only-integers.js
+++ 250820/정수만 추출하기/extract-only-integers.js
@@ -1,7 +1,19 @@
 const fs = require('fs');
-const input = fs.readFileSync(0).toString().trim();
+const input = fs.readFileSync(0).toString().trim().split(' ');
 
-const result = [...input.matchAll(/(\d+)(?=[\p{P}\p{S}])/gu)]
-  .map(m => Number(m[1]));
+let idx = 0;
+let result = 0;
 
-console.log(result[0] + result[1]);
+for(let str of input){
+  for(let s of str){
+    if(s <= '9' && s >= '0') idx++;
+    else break;
+  }
+
+  result += Number(str.slice(0, idx));
+  idx = 0;
+}
+
+console.log(result);
+
+
