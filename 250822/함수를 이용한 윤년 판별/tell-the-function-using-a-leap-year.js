145dbe6e0b8360517756894ea5737635fad5c11e
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2025-08-22 19:08:18 +0900

    Title: 함수를 이용한 윤년 판별, Time: 174ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250822/함수를 이용한 윤년 판별/tell-the-function-using-a-leap-year.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const y = Number(input[0]);
+// Please Write your code here.
+
+function isLeapYear(n) {
+    if (n % 100 === 0 && n % 400 !== 0) {
+        return false;
+    }
+
+    if (n % 4 === 0) {
+        return true;
+    }
+
+    return false;
+}
+
+console.log(isLeapYear(y));
