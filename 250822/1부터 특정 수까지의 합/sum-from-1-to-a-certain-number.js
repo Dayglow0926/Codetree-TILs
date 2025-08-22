2f13413c0640ef68316242954276278428374229
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2025-08-22 17:39:11 +0900

    Title: 1부터 특정 수까지의 합, Time: 170ms, Memory: 15MB, Status: Passed - Codetree

--- 250822/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.js
+++ 250822/1부터 특정 수까지의 합/sum-from-1-to-a-certain-number.js
@@ -4,7 +4,7 @@ const n = Number(input[0]);
 // Please Write your code here.
 
 function sum(n){
-    return (1 + n) * (n / 2) + (n % 2 && Math.ceil(n / 2))
+    return (1 + n) * Math.floor(n / 2) + (n % 2 && Math.ceil(n / 2))
 }
 
 console.log(Math.floor(sum(n) / 10));
