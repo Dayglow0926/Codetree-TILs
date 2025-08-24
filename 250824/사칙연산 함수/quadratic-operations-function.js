692da73c295644d52528c55c7d3ae1bffac793aa
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2025-08-24 13:47:42 +0900

    Title: 사칙연산 함수, Time: 171ms, Memory: 15MB, Status: Wrong Answer - Codetree

--- 250824/사칙연산 함수/quadratic-operations-function.js
+++ 250824/사칙연산 함수/quadratic-operations-function.js
@@ -21,9 +21,9 @@ function div(a,b){
     return a / b;
 }
 
-if(o === '+') console.log(add(a,b));
-else if(o === '-') console.log(minus(a,b));
-else if(o === '*') console.log(mul(a,b));
-else if(o === '/') console.log(div(a,b));
+if(o === '+') console.log(`${a} ${o} ${c} = ${add(a,c)}`);
+else if(o === '-') console.log(`${a} ${o} ${c} = ${minus(a,c)}`);
+else if(o === '*') console.log(`${a} ${o} ${c} = ${mul(a,c)}`);
+else if(o === '/') console.log(`${a} ${o} ${c} = ${div(a,c)}`);
 else console.log("False");
 
