d68c37d66c0ac71c91ff51072a0af5574f4c0041
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2025-08-31 15:22:07 +0900

    Title: k번째로 신기한 문자열, Time: 173ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250831/k번째로 신기한 문자열/kth-special-string.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+let index = 0;
+const [n, k, t] = input[index++].split(' ');
+const words = [];
+for (let i = 0; i < Number(n); i++) {
+    words.push(input[index++]);
+}
+// Please Write your code here.
+
+const filter = words.filter((s) => s.indexOf(t) == 0).sort();
+
+console.log(filter[k-1]);
