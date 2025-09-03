8c26b10c499f99f6a3b34aa5673ad85a74ae6cf3
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2025-09-03 11:52:39 +0900

    Title: 원점으로부터의 거리, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250903/원점으로부터의 거리/distance-from-origin.js
@@ -0,0 +1,30 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const points = input.slice(1).map(line => line.trim().split(' ').map(Number));
+
+// Please Write your code here.
+class Dot{
+    constructor(x, y, i){
+        this.x = x;
+        this.y = y;
+        this.i = i;
+    }
+}
+
+const list = []
+points.forEach((p, i) => {
+    list.push(new Dot(p[0], p[1], i+1));
+})
+
+list.sort((p, c) => {
+    const dot = (p.x + p.y) - (c.x + c.y);
+    if(dot === 0) return p.i - c.i;
+
+    return dot;
+})
+
+list.forEach((d) => {
+    console.log(d.i)
+})
