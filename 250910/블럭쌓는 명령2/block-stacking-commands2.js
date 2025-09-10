bcafd36158977c148619d76d2dccc44c59f945d8
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2025-09-10 11:20:59 +0900

    Title: 블럭쌓는 명령2, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 250910/블럭쌓는 명령2/block-stacking-commands2.js
+++ 250910/블럭쌓는 명령2/block-stacking-commands2.js
@@ -5,7 +5,7 @@ const segments = input.slice(1, k + 1).map(line => line.split(' ').map(Number));
 
 // Please write your code here.
 
-const arr = Array(n).fill(0);
+const arr = Array(n+1).fill(0);
 
 segments.forEach((segment) => {
     for(let i = segment[0]; i<= segment[1]; i++){
