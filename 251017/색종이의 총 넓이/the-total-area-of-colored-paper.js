6ac37018dd9e6ef258ee1001db58ee2e97ec523f
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2025-10-17 22:39:12 +0900

    Title: 색종이의 총 넓이, Time: 70ms, Memory: 10MB, Status: Passed - Codetree

--- 251017/색종이의 총 넓이/the-total-area-of-colored-paper.js
+++ 251017/색종이의 총 넓이/the-total-area-of-colored-paper.js
@@ -5,7 +5,8 @@ const n = Number(input[0]);
 const rects = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
 
 // Please Write your code here.
-const SIZE = 100;
+const OFFSET = 100
+const SIZE = 201;
 const LEN = 8;
 
 const grid = Array.from({length: SIZE}, () => Array(SIZE).fill(0));
@@ -13,10 +14,12 @@ const grid = Array.from({length: SIZE}, () => Array(SIZE).fill(0));
 let area = 0;
 
 for(let rect of rects){
+    const x = rect[0]+OFFSET, y = rect[1]+OFFSET;
+
     for(let i=1; i<=LEN; i++ ){
         for(let j=1; j<=LEN; j++ ){
-            if(grid[rect[0]+i][rect[1]+j] === 0){
-                grid[rect[0]+i][rect[1]+j] = 1;
+            if(grid[x+i][y+j] === 0){
+                grid[x+i][y+j] = 1;
                 area++;
             }
         }
