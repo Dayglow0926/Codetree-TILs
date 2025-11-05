7cef7a3293639609824ffff345fc3e52820e18e9
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2025-11-05 23:59:53 +0900

    Title: 연속되는 수 2, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 251105/연속되는 수 2/continuous-number2.js
+++ 251105/연속되는 수 2/continuous-number2.js
@@ -5,11 +5,15 @@ const n = Number(input[0]);
 const arr = input.slice(1, n + 1).map(Number);
 
 // Please Write your code here.
+if(arr.length <= 1){
+    console.log(1);
+    return ;
+}
 
 let result = 0;
 let cnt = 0;
 
-for(let i=1; i<arr.length ;i++){
+for(let i=1; i<arr.length; i++){
     if(arr[i-1] !== arr[i]){
         if(result < cnt) result = cnt + 1;
         cnt = 0;
