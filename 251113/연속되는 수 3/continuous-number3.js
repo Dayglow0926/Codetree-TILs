54120350f914fb9c89b186d272ad070faa093e01
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2025-11-13 21:11:34 +0900

    Title: 연속되는 수 3, Time: 62ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251113/연속되는 수 3/continuous-number3.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const arr = input.slice(1, 1 + n).map(Number);
+
+// Please Write your code here.
+
+let result = 0;
+let cnt = 0;
+
+for(let num of arr){
+    if(num < 0){
+        if(cnt > 0){
+            result = result < Math.abs(cnt) ? Math.abs(cnt) : result;
+            cnt = 0;
+        }
+        cnt --;
+    }else{
+        if(cnt < 0){
+            result = result < Math.abs(cnt) ? Math.abs(cnt) : result;
+            cnt = 0;
+        }
+        cnt ++;
+    }
+}
+
+console.log(result);
