b6a1305739e9813ec3da8c6774d978eb8d009b93
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2025-12-13 20:14:29 +0900

    Title: 구간 중 최대 합, Time: 67ms, Memory: 10MB, Status: Passed - Codetree

--- 251213/구간 중 최대 합/max-sum-of-subarray.js
+++ 251213/구간 중 최대 합/max-sum-of-subarray.js
@@ -8,7 +8,7 @@ const arr = input[1].split(' ').map(Number);
 
 let result = 0;
 
-for(let i=0; i<n-k; i++){
+for(let i=0; i<=n-k; i++){
     result = Math.max(result, arr.slice(i, i+k).reduce((p,c) => p+c,0));
 }
 
