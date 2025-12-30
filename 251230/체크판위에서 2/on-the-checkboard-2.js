68735ead6082d14c7b0c2d14b4281dce9b2b2202
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2025-12-30 19:42:27 +0900

    Title: 체크판위에서 2, Time: 66ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 251230/체크판위에서 2/on-the-checkboard-2.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [R, C] = input[0].split(' ').map(Number);
+const grid = input.slice(1).map(line => line.trim().split(' '));
+
+// Please Write your code here.
+
+let count = 0;
+//row, col, color
+let color = grid[0][0];
+const now = [0,0]
+
+for(let row=1; row<R; row++){
+    for(let col=1; col<C; col++){
+        if(grid[0][0] !== grid[row][col]){
+            for(let row2=row+1; row2<R-1; row2++){
+                for(let col2=col+1; col2<C-1; col2++){
+                    if(grid[row][col] !== grid[row2][col2]){
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
+
+console.log(count);
