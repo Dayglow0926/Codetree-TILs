831e29da3c1b2ba0d69334294c6636c2fd42ed07
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2026-01-09 00:15:14 +0900

    Title: 빙빙 돌며 사각형 채우기, Time: 65ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260109/빙빙 돌며 사각형 채우기/snail-alphabet-square.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, m] = input[0].split(' ').map(Number);
+// Please Write your code here.
+
+function check(nx, ny) {
+    return nx >= 0 && nx < m && ny >= 0 && ny < n;
+}
+
+const arr = Array.from({ length: n }, () => Array.from({ length: m }, () => 0))
+let dirNum = 0;
+const dx = [1, 0, -1, 0], dy = [0, 1, 0, -1]
+let x = 0, y = 0;
+
+arr[x][y] = String.fromCharCode(65);
+
+const code = 65;
+
+for (let i = 1; i <= (n * m)-1; i++) {
+    const nx = x + dx[dirNum], ny = y + dy[dirNum];
+
+    if (!check(nx, ny) || arr[ny][nx] !== 0) {
+        dirNum = (dirNum + 1) % 4;
+    }
+
+    x += dx[dirNum];
+    y += dy[dirNum];
+
+    
+    arr[y][x] = String.fromCharCode(code + (i % 26));
+}
+
+arr.forEach((v) => console.log(v.join(' ')))
