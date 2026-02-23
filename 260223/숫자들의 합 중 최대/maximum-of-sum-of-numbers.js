d49eaad2c33c5ce9ab71fd59f38d03117b19ec90
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2026-02-23 23:40:59 +0900

    Title: 숫자들의 합 중 최대, Time: 68ms, Memory: 10MB, Status: Passed - Codetree

--- 260223/숫자들의 합 중 최대/maximum-of-sum-of-numbers.js
+++ 260223/숫자들의 합 중 최대/maximum-of-sum-of-numbers.js
@@ -7,20 +7,9 @@ const [x, y] = input[0].split(' ').map(Number);
 let max = -1;
 
 for(let i=x; i<=y; i++){
-    const arr = Array.from(String(i), Number);
+    const sum = Array.from(String(i), Number).reduce((c, p) => c+p, 0);
 
-    if(arr.length<2){
-        max = Math.max(max, i);
-        continue;
-    }
-
-    for(let a=0; a<arr.length; a++){
-        for(let b=a+1; b<arr.length;b++){
-            let sum = arr[a] + arr[b];
-
-            max = Math.max(max, sum);
-        }
-    }
+    max = Math.max(max, sum)
 }
 
 console.log(max)
