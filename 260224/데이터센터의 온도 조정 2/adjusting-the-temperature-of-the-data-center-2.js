0a8b94a2b53b2ef271a3cc0f24d004c72d75bee8
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2026-02-24 00:00:15 +0900

    Title: 데이터센터의 온도 조정 2, Time: 65ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260224/데이터센터의 온도 조정 2/adjusting-the-temperature-of-the-data-center-2.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const [n, c, g, h] = input[0].split(' ').map(Number);
+const ta = [], tb = [];
+let tMin = 1000, tMax = 0;
+
+for (let i = 1; i <= n; i++) {
+    const [taValue, tbValue] = input[i].split(' ').map(Number);
+    ta.push(taValue);
+    tMin = Math.min(tMin, taValue);
+
+    tb.push(tbValue);
+    tMax = Math.max(tMax, tbValue);
+}
+
+// Please Write your code here.
+
+let max = -1;
+
+for(let i=tMin; i<=tMax; i++){
+    let sum = 0;
+
+    for(let j=0; j<ta.length; j++){
+        if(i < ta[j]){
+            sum += c;
+        }else if(i > tb[j]){
+            sum += h;
+        }else{
+            sum += g;
+        }
+    }
+
+    max = Math.max(max, sum);
+}
+
+console.log(max);
