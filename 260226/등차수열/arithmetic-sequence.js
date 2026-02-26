f5bc1cfa2b7ad20e7b0e5db0785ebd13400e2ed9
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2026-02-26 23:44:34 +0900

    Title: 등차수열, Time: 69ms, Memory: 12MB, Status: Passed - Codetree

--- 260226/등차수열/arithmetic-sequence.js
+++ 260226/등차수열/arithmetic-sequence.js
@@ -2,20 +2,24 @@ const fs = require("fs");
 const input = fs.readFileSync(0).toString().trim().split('\n');
 
 const n = Number(input[0]);
-const arr = input[1].split(' ').map(Number).sort((a,b) => a-b);
+const arr = input[1].split(' ').map(Number).sort((a, b) => a - b);
 
 // Please Write your code here.
 
-let result = 0;
+let max = 0;
 
-for(let k=arr[0]+1; k<arr[arr.length-1]; k++){
-    for(let x=0; x<arr.length-1; x++){
-        for(let y=x+1; y<arr.length; y++){
-            if(arr[x] - k === k - arr[y]){
+for (let k = arr[0] + 1; k < arr[arr.length - 1]; k++) {
+    let result = 0;
+
+    for (let x = 0; x < arr.length - 1; x++) {
+        for (let y = x + 1; y < arr.length; y++) {
+            if (arr[y] - k === k - arr[x]) {
                 result++;
             }
         }
     }
+
+    max = Math.max(max, result);    
 }
 
-console.log(result);
+console.log(max);
