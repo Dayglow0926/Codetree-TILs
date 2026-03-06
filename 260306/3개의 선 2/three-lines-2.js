20de6456b775eb00ef5602b410b979b15135b362
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2026-03-06 23:41:21 +0900

    Title: 3개의 선 2, Time: 67ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 260306/3개의 선 2/three-lines-2.js
@@ -0,0 +1,29 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+
+const n = Number(input[0]);
+const points = input.slice(1, 1 + n).map(line => line.split(' ').map(Number));
+
+// Please Write your code here.
+const LINE_LIMIT=3;
+
+
+for(let j=0; j<=LINE_LIMIT; j++){
+    const temp = [...points]
+
+    for(let xl=0; xl<LINE_LIMIT-j; xl++){
+        const num = temp[0];
+        temp.filter((v) => v[0] !== num[0]);
+    }
+
+    for(let yl=0; yl<j; yl++){
+        const num = temp[0];
+        temp.filter((v) => v[1] !== num[1]);
+    }
+
+    console.log(temp);
+    
+    if(temp.length === 0) console.log(1); return;
+}
+
+console.log(0);
