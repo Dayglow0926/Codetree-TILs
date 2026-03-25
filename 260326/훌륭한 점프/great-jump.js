7e222133c42533d7e3230c504c45b42cf8b8262f
KIM JOO KWANG <41177346+Dayglow0926@users.noreply.github.com>
2026-03-26 00:22:44 +0900

    Title: 훌륭한 점프, Time: 70ms, Memory: 11MB, Status: Wrong Answer - Codetree

---
+++ 260326/훌륭한 점프/great-jump.js
@@ -0,0 +1,37 @@
+const fs = require("fs");
+const input = fs.readFileSync(0).toString().trim().split('\n');
+const [n, k] = input[0].split(' ').map(Number);
+const arr = input[1].split(' ').map(Number);
+// Please Write your code here.
+const MAX = Math.max(arr[0], arr[arr.length - 1]);
+const NUMBER_MAX = Math.max(...arr);
+let result = Number.MAX_SAFE_INTEGER;
+
+for(let i = NUMBER_MAX; i>MAX; i--){
+    if(arr.indexOf(i) < 0 ) continue;
+
+    const array = [];
+
+    arr.forEach((elem, index) => {
+        if(elem <= i){
+            array.push(index);
+        }
+    })
+
+    let lenCheck = true;
+
+    for(let j=0; j<array.length-1; j++){
+        const dist = array[j+1] - array[j];
+
+        if(dist > k) {
+            lenCheck = false;
+            break;
+        }
+    }
+
+    if(lenCheck){
+        result = Math.min(result, i);
+    }
+}
+
+console.log(result);
